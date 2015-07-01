e386b9df72c2057362bf4266a79c54c7c2e04c36
SadhanaG <sadhana.gogoi1@gmail.com>
2015-07-01 16:35:00 +0530

    Adds Date.js
    Shows the system day in the file

--- components/date.js
+++ components/date.js
@@ -1,9 +1,21 @@
+
 var express = require('express');
 var app = express();
 var ndate = new Date();
 
+var ndate = new Date();
+var ndays = new Array(7);
+    ndays[0] = 'Sunday';
+    ndays[1] = 'Monday';
+    ndays[2] = 'Tuesday';
+    ndays[3] = 'Wednesday';
+    ndays[4] = 'Thursday'
+    ndays[5] = 'Friday';
+    ndays[6] = 'Saturday';
+var d = ndays[ndate.getDay()] ;
+
 app.use(function (req, res, next) {
-  res.send('Time:', ndate.toDateString());
+  res.send('Time:', d);
   next();
 });
 
