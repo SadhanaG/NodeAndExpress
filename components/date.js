51fb2a21b14397eca210e7a162cb6b1be0abd3bd
SadhanaG <sadhana.gogoi1@gmail.com>
2015-07-01 16:47:50 +0530

    Adds comments in Date.js
    Describe the function by comments.

--- components/date.js
+++ components/date.js
@@ -1,9 +1,9 @@
-
+// First thing we need to do is require express, and create an app
 var express = require('express');
 var app = express();
+// Second create a variable to call the system Date which contain the days too.
 var ndate = new Date();
-
-var ndate = new Date();
+// To show the days, we need to create an Array
 var ndays = new Array(7);
     ndays[0] = 'Sunday';
     ndays[1] = 'Monday';
@@ -14,16 +14,15 @@ var ndays = new Array(7);
     ndays[6] = 'Saturday';
 var d = ndays[ndate.getDay()] ;
 
+// A middleware with no mount path; gets executed for every request to the app
 app.use(function (req, res, next) {
   res.send('Time:', d);
   next();
 });
 
+//Send the HTTP request 
 var server = app.listen(3000, function () {
-
   var host = server.address().address;
   var port = server.address().port;
-
   console.log('Example app listening at http://%s:%s', host, port);
-
 });
