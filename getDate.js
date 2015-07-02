ee5b10c45cfb4024002cbb406261ff8c0a28c9a6
SadhanaG <sadhana.gogoi1@gmail.com>
2015-07-02 17:40:54 +0530

    Adds comments
    Comments the statements and their rules.

--- getDate.js
+++ getDate.js
@@ -1,15 +1,21 @@
+// First thing we need to do is require express, and create an app
 var express = require('express');
 var app = express();
+// Required extra middleware layer as a body-parser to handle the POST request
 var bodyParser = require('body-parser');
+//Configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded());
 
-app.use(bodyParser.urlencoded({ extended:false }));
+//express.static is start serving the files directly from the directory name public
 app.use(express.static('public'));
 
+//POST the request to server and get the response
 app.post("/callDate",function(req, res){
   var selDate =  req.body.selectDate;
   res.send(selDate);
 });
 
+//Start the server
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
